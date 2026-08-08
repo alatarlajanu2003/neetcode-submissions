d0815c7600e74509e93fe1669578d156ace35a01
Ala Tarlajanu <53252476+alatarlajanu2003@users.noreply.github.com>
2026-08-08 17:02:45 +0200

    Add: valid-sudoku - submission-23

---
+++ Data Structures & Algorithms/valid-sudoku/submission-23.py
@@ -0,0 +1,72 @@
+# 1: 00
+# 2: 01
+# 3: 04
+# 4: 10
+# 5: 13
+# 9: 21
+
+# 00 01; 00 02; 00 03
+# 01; 02; 03
+
+# 00 10; 00 11; 00 12
+# 10; 11, 12
+
+# 00 20; 00 21; 00 22
+# 20; 21; 22
+
+# for the same num:
+
+# |x1 - x2| > 3 || |y1 - y2| > 3
+# if holds -> update latest position
+# if not -> return false
+
+# if x1 == x2 or y1 == y2 -> return false
+
+# return true in the end
+
+# how to identify if they are in the same square?
+
+from collections import defaultdict
+
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        num_to_position = defaultdict(list)
+
+        for row_index in range(len(board)):
+            for column_index in range(len(board[row_index])):
+                
+                num = board[row_index][column_index]
+                if not num.isnumeric():
+                    continue
+
+                positions_of_num = num_to_position[num]
+
+                if not positions_of_num:
+                    num_to_position[num].append((row_index, column_index))
+                else:
+                    for (x_pos, y_pos) in positions_of_num:
+                        # row
+                        if row_index == x_pos:
+                            return False
+
+                        # column
+                        if column_index == y_pos:
+                            return False
+
+                        # 3 x 3
+                        if abs(row_index - x_pos) <= 2 and abs(column_index - y_pos) <= 2 and ((row_index // 3, column_index // 3) == (x_pos // 3, y_pos // 3)):
+                            return False
+
+                    num_to_position[num].append((row_index, column_index))
+
+        return True
+
+
+
+
+
+
+
+
+
+
